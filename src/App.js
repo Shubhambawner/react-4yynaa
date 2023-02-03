ec601bb7e02d980294ab5550b668b081e612cd3c
Shubhambawner <wurtzreaxnwittigreaxn@gmail.com>
2023-02-04 01:18:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './style.css';
+import dicomParser from 'dicom-parser';
+import cornerstone from 'cornerstone-core';
+import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import cornerstoneMath from 'cornerstone-math';
+import cornerstoneTools from 'cornerstone-tools';
+import Hammer from 'hammerjs';
+import CornerstoneViewport from 'react-cornerstone-viewport';
+
+export default function App() {
+  state = {
+    tools: [
+      // Mouse
+      {
+        name: 'Wwwc',
+        mode: 'active',
+        modeOptions: { mouseButtonMask: 1 },
+      },
+      {
+        name: 'Zoom',
+        mode: 'active',
+        modeOptions: { mouseButtonMask: 2 },
+      },
+      {
+        name: 'Pan',
+        mode: 'active',
+        modeOptions: { mouseButtonMask: 4 },
+      },
+      // Scroll
+      { name: 'StackScrollMouseWheel', mode: 'active' },
+      // Touch
+      { name: 'PanMultiTouch', mode: 'active' },
+      { name: 'ZoomTouchPinch', mode: 'active' },
+      { name: 'StackScrollMultiTouch', mode: 'active' },
+    ],
+    imageIds: [
+      'dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.11.dcm',
+      'dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.12.dcm',
+    ],
+  };
+
+  return (
+    <div>
+      <h1>Hello Sample using cornerstonejs</h1>
+      <CornerstoneViewport
+        tools={this.state.tools}
+        imageIds={this.state.imageIds}
+        style={{ minWidth: '100%', height: '512px', flex: '1' }}
+      />
+    </div>
+  );
+}
